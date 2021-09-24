9706ba61c2c7e48a81c94db479b6c791f79c0e4f
oliviazg <ozehgn30@stuy.edu>
2021-09-23 21:19:20 -0400

    Replaced fraction

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,7 +1,7 @@
 // The functions should have temperatures as parameters. The type should be double. The functions should return the temperature as Celsius if the temperature is in Fahrenheit, and in Fahrenheit if the temperature is in Celsius.
 public class CtoFTester {
 	public static double celsiusToFahrenheit(double a){
-		return ( a * ( 9.0 / 5.0 ) ) + 32.0;
+		return ( a * 1.8 ) + 32.0;
 	}
 	public static double fahrenheitToCelcius(double b){
 		return ( b - 32.0 ) * ( 5.0 / 9.0 );

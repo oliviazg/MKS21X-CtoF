22c6d361fc64be0adae0c61ad01a6bd8cee0c7b8
oliviazg <ozehgn30@stuy.edu>
2021-10-01 07:04:00 -0400

    First draft

---
+++ ArrayTest.java
@@ -0,0 +1,14 @@
+public class ArrayTest{
+   public static void main(String[]args){
+        int[] anArray = new int[11];
+	for (int x = 0; x < 11; x++){
+		anArray[x] = 100 + x * 10;
+	}
+        System.out.println("part 1:");
+        System.out.print(anArray);
+        System.out.println("part 2:");
+        for (int y = 0; y < 11; y++){
+		System.out.print(anArray[x]);
+	}
+   }
+}

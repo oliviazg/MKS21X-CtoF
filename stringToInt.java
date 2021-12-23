846c64332776beac4831e80c30979116665caf09
oliviazg <ozehgn30@stuy.edu>
2021-12-23 10:39:48 -0500

    wrote valueOfDigit

--- stringToInt.java
+++ stringToInt.java
@@ -1,5 +1,7 @@
 public static int stringToInt(String s){
-  
   //return the value of Integer.parseInt(s) but do it yourself!
   //Do not use any built in parse methods.
+  public int valueOfDigit(char c){
+    return (int)c;
+  }
 }

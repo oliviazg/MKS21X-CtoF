f42becd6f68538e6214d5db510c361dd80c2e286
oliviazg <ozehgn30@stuy.edu>
2021-12-23 10:43:39 -0500

    Wrote main for parseToInt

--- stringToInt.java
+++ stringToInt.java
@@ -4,4 +4,11 @@ public static int stringToInt(String s){
   public int valueOfDigit(char c){
     return (int)c;
   }
+  public static void main (String[] args){
+    int ans = -1;
+    for (int i = 0; i < args[0].length(); i++){
+      digit = args[0][i];
+      ans += (int)digit * Math.pow(10, args[0].length() - 1);
+    }
+  }
 }

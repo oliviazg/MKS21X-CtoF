947487940e5e2f004e50fed8b9242602e5b4e3aa
oliviazg <ozehgn30@stuy.edu>
2021-12-23 11:02:02 -0500

    Resolved typecasting issues

--- parseInt.java
+++ parseInt.java
@@ -1,19 +1,15 @@
 public class parseInt{
-  public int valueOfDigit(char c){
-    return (int)c;
-  }
   public static int stringToInt(String s){
     //return the value of Integer.parseInt(s) but do it yourself!
     //Do not use any built in parse methods.
     int ans = 0;
     for (int i = 0; i < s.length(); i++){
-      int digit = s.charAt(i);
-      ans += (((int)digit) * Math.pow(10, s.length() - i - 1));
+      int digit = (int)(s.charAt(i)) - 48;
+      ans += (digit * Math.pow(10, s.length() - i - 1));
     }
     return ans;
   }
   public static void main (String[] args){
-    String tester = "9123";
-    System.out.println(stringToInt(tester));
+    System.out.println(stringToInt("12345"));
   }
 }
